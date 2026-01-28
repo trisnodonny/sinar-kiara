c21fbebd93e0e5302bb704385ea3018efcb21eb8
trisnodonny <trisnodonny@gmail.com>
2026-01-28 14:57:24 +0700

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,16 +15,16 @@ export default function Navbar({ anchor }: NavbarProps) {
     { label: "Contact", target: "contact" }
   ];
 
-  const [width, setWidth] = useState(
-    typeof window !== "undefined" ? window.innerWidth : 0,
-  );
+  // const [width, setWidth] = useState(
+  //   typeof window !== "undefined" ? window.innerWidth : 0,
+  // );
 
-  useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth);
-    window.addEventListener("resize", handleResize);
+  // useEffect(() => {
+  //   const handleResize = () => setWidth(window.innerWidth);
+  //   window.addEventListener("resize", handleResize);
 
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  //   return () => window.removeEventListener("resize", handleResize);
+  // }, []);
 
   const [isScrolled, setIsScrolled] = useState(false);
   let navBgColor = isScrolled ? "is-scrolled" : "";
